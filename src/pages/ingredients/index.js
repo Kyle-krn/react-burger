7e32d5ca22beee2ea965f28b097d71d5a50411a9
Egor Egorov <egor.egorov@rt.ru>
2024-07-23 19:52:09 +0300

    Данные берутся по API

--- src/pages/ingredients/index.js
+++ src/pages/ingredients/index.js
@@ -1,15 +1,47 @@
+import { useEffect, useState } from "react";
 import BurgerIngredients from "../../components/burger-ingredients";
 import BurgerConstructor from "../../components/burger-constructor";
 import styles from './styles.module.css'
-import { INGREDIENT_DATA } from "../../utils/data";
+
+const API_URL = 'https://norma.nomoreparties.space/api/ingredients'
 
 const IngredientsPage = () => {
+    const [state, setState] = useState({
+        ingredientData: [],
+        isLoading: false,
+        error: null,
+    })
+
+    useEffect(() => {
+        const getIngredients = async () => {
+            try {
+                setState({ ...state, isLoading: true, error: null });
+                const res = await fetch(API_URL);
+                const data = await res.json();
+                setState({
+                    ingredientData: data.data,
+                    isLoading: false,
+                    error: null,
+                });
+            } catch (error) {
+                setState({...state, isLoading: false, error: error.message });                
+            }
+        }
+        getIngredients();
+    }, []); 
     return (
         <>
             <h1 className="text-align-l mt-10 mb-5 text_type_main-large">Соберите бургер</h1>
             <div className={styles.wrapper}>
-                <BurgerIngredients ingredientData={INGREDIENT_DATA}/>
-                <BurgerConstructor ingredientData={INGREDIENT_DATA}/>
+                {
+                state.isLoading === false && state.error === null && state.ingredientData.length > 0? 
+                <>
+                    <BurgerIngredients ingredientData={state.ingredientData}/>
+                    <BurgerConstructor ingredientData={state.ingredientData}/>
+                </>
+                : 
+                null
+            }
             </div>
         </>
     );
