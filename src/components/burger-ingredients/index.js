1bbbbf95122812e171806281593fd4d269dd38de
Egor Egorov <egor.egorov@rt.ru>
2024-08-02 13:40:21 +0300

    Переключение tab

--- src/components/burger-ingredients/index.js
+++ src/components/burger-ingredients/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import PropTypes from "prop-types";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import IngredientsList from "../ingredients-list";
@@ -11,20 +11,28 @@ import { getIngredients } from "../../services/indgredients";
 import { setIngredient, resetIngredient } from "../../services/ingredient-detail";
 
 const BurgerIngredients = () => {
-    const ingredients = useSelector(state => state.ingredients.ingredients);
-
     const dispatch = useDispatch();
-
-    const [current, setCurrent] = useState('bun');
+    
+    const ingredients = useSelector(state => state.ingredients.ingredients);
     const selectedIngredient = useSelector(state => state.ingredientDetail.ingredient);
+    const [current, setCurrent] = useState('bun');
+
+    const tabsRef = {
+        bun: useRef(null),
+        sauce: useRef(null),
+        main: useRef(null),
+    };
+    const containerRef = useRef(null);
+
 
     useEffect(() => {
         dispatch(getIngredients());
     }, [dispatch]);
 
-    useEffect(() => {
-		document.querySelector(`#${current}`)?.scrollIntoView({behavior: 'smooth'});
-	},[current]);
+    const setTab = (value) => {
+        setCurrent(value);
+        tabsRef[value].current?.scrollIntoView({behavior: 'smooth'});
+    }
 
     const handlerOpenInfo = useCallback((ingredientId) => {
         const ingredientDetail = ingredients.filter(item => item._id === ingredientId);
@@ -34,24 +42,41 @@ const BurgerIngredients = () => {
     const handlerCloseInfo = useCallback(() => {
         dispatch(resetIngredient());
     }, [dispatch, resetIngredient]);
+
+    const handleScroll = () => {
+        const bunPosition = tabsRef.bun.current.getBoundingClientRect().top - containerRef.current.getBoundingClientRect().top;
+        const saucePosition = tabsRef.sauce.current.getBoundingClientRect().top - containerRef.current.getBoundingClientRect().top;
+        const mainPosition = tabsRef.main.current.getBoundingClientRect().top - containerRef.current.getBoundingClientRect().top;
+
+        const positions = [
+            { section: 'bun', position: bunPosition },
+            { section: 'sauce', position: saucePosition },
+            { section: 'main', position: mainPosition }
+        ];
+        
+        const closestSection = positions.reduce((prev, curr) => {
+            return Math.abs(curr.position) < Math.abs(prev.position) ? curr : prev;
+        });
+        setCurrent(closestSection.section);
+    }
     
     return (
         <section className="max-width-600">
             <div className="d-flex">
-                <Tab value="bun" active={current === 'bun'} onClick={setCurrent}>
+                <Tab value="bun" active={current === 'bun'} onClick={setTab}>
                     Булки
                 </Tab>
-                <Tab value="sauce" active={current === 'sauce'} onClick={setCurrent}>
+                <Tab value="sauce" active={current === 'sauce'} onClick={setTab}>
                     Соусы
                 </Tab>
-                <Tab value="main" active={current === 'main'} onClick={setCurrent}>
+                <Tab value="main" active={current === 'main'} onClick={setTab}>
                     Начинки
                 </Tab>
             </div>
-            <div className={`custom-scroll ${styles.ingredientsScroll}`}>
-                <IngredientsList id='bun' ingredients={ingredients.filter(item => item.type === 'bun')} title="Булки" onClick={handlerOpenInfo}/>
-                <IngredientsList id='sauce' ingredients={ingredients.filter(item => item.type === 'sauce')} title="Соусы" onClick={handlerOpenInfo}/>
-                <IngredientsList id='main' ingredients={ingredients.filter(item => item.type === 'main')} title="Начинки" onClick={handlerOpenInfo}/>
+            <div className={`custom-scroll ${styles.ingredientsScroll}`} ref={containerRef} onScroll={handleScroll}>
+                <IngredientsList subRef={tabsRef.bun} id='bun' ingredients={ingredients.filter(item => item.type === 'bun')} title="Булки" onClick={handlerOpenInfo}/>
+                <IngredientsList subRef={tabsRef.sauce} id='sauce' ingredients={ingredients.filter(item => item.type === 'sauce')} title="Соусы" onClick={handlerOpenInfo}/>
+                <IngredientsList subRef={tabsRef.main} id='main' ingredients={ingredients.filter(item => item.type === 'main')} title="Начинки" onClick={handlerOpenInfo}/>
             </div>
             {selectedIngredient && 
                 <Modal title="Детали ингредиента" onClose={handlerCloseInfo}>
--- src/components/ingredients-list/index.js
+++ src/components/ingredients-list/index.js
@@ -1,11 +1,12 @@
+import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.module.css';
 import IngredientsItem from "../ingredients-item";
 
-const IngredientsList = ({title, ingredients, id, onClick}) => {
+const IngredientsList = ({title, ingredients, id, onClick, subRef}) => {
     return (
         <div className="mt-10" id={id}>
-            <p className={`text-align-l mb-6 text_type_main-medium`}>{title}</p>  
+            <p className={`text-align-l mb-6 text_type_main-medium`} ref={subRef}>{title}</p>  
             <div className={styles.cardsWrapper}>
                 {ingredients.map(item => <IngredientsItem key={item._id} name={item.name} price={item.price} image={item.image} count={0} id={item._id} onClick={onClick}/>)}
             </div>
@@ -18,6 +19,7 @@ IngredientsList.propTypes = {
     ingredients: PropTypes.arrayOf(PropTypes.object).isRequired,
     id: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
+    // subRef: PropTypes.ref,
 }
 
 export default IngredientsList;
